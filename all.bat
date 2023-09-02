018be8af9681c0ad7db7db6d9cd72664e1caccd5
Aborres <jmnaranjotemprano@gmail.com>
2023-09-02 21:44:11 +0200

    Adding Nvidia bat file

--- all.bat
+++ all.bat
@@ -1,8 +1,18 @@
 @echo off 
 
+echo "Installing main"
 call "scripts/main.bat"
+
+echo "Installing Nvidia"
+call "scripts/nvidia.bat"
+
+echo "Installing media"
 call "scripts/media.bat"
+
+echo "Installing dev"
 call "scripts/dev.bat"
+
+echo "Installing Gaming"
 call "scripts/gaming.bat"
 
 echo "Done"
---
+++ scripts/nvidia.bat
@@ -0,0 +1,3 @@
+@echo off
+
+choco install geforce-experience
